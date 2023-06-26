0d75d0cceb20c16ece81f4b08d850cd61469ac35
Sarvnaz K <sarvnaz.kasaei@edu.cmeducations.se>
2023-06-26 13:31:05 +0200

    styling

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,5 +1,45 @@
+import React from "react";
 import Heading from "@components/heading";
 
+
 export default function About() {
-  return <Heading>About</Heading>;
+  return (
+    <div>
+      <Heading>About</Heading>
+      <p>Welcome to Feel Good Stories!</p>
+      <p>
+        At Feel Good Stories, we believe in the power of storytelling to inspire,
+        uplift, and bring joy to people's lives. Our blog is a sanctuary for those
+        seeking a momentary escape from the challenges of everyday life, where they
+        can immerse themselves in a world of inspiring and uplifting short stories.
+      </p>
+      <p>
+        We believe that stories have the ability to touch hearts, ignite imaginations,
+        and evoke a range of emotions. Our team of passionate writers craft each tale
+        with care, ensuring that every word resonates and leaves a lasting impact.
+        Through our stories, we aim to create moments of pure happiness, where readers
+        can experience the magic of imagination and connect with the inherent goodness
+        of humanity.
+      </p>
+      <p>
+        Whether you are looking for a heartwarming story to brighten your day, a tale
+        of courage and resilience to inspire you, or simply a moment of escape into a
+        world filled with hope, Feel Good Stories is here for you. Our stories
+        transcend boundaries and touch upon universal themes that remind us of our
+        shared humanity.
+      </p>
+      <p>
+        Join us on this journey as we explore the depths of human emotions, the
+        triumph of the human spirit, and the beauty that exists in everyday life. We
+        invite you to dive in and immerse yourself in our collection of inspiring
+        short stories. Let each word transport you to a place of warmth and positivity,
+        leaving you with a renewed sense of joy and a smile on your face.
+      </p>
+      <p>
+        Thank you for visiting Feel Good Stories. We hope you find solace, inspiration,
+        and above all, a reason to believe that good things are possible.
+      </p>
+      <p>Happy reading!</p>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
-import Heading from "@components/heading";
+import React from 'react';
+import Link from 'next/link'; // Import Link from next/link
+import styles from './Home.module.css';
 
 export default function Home() {
-  return <Heading>Home</Heading>;
-  
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.heading}>Welcome to Feel Good Stories!</h1>
+            <h2 className={styles.subHeading}>Home of Inspiring Short Stories</h2>
+            <p className={styles.paragraph}>
+                Dive into a world full of inspiring and uplifting short stories. Each tale is crafted to 
+                uplift your spirit, ignite your imagination, and make you feel good. 
+                It's not just about reading, but about experiencing and feeling each word. Enjoy the journey!
+            </p>
+            <Link href="/blog" passHref>
+                <button className={styles.diveInButton}>Dive In</button>
+            </Link>
+        </div>
+    );
 }
+
+
+
